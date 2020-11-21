3e50306665d4ea45e7e71140eb0b8413d516e2e4
testbora <boraozgurr@outlook.com>
2020-11-21 15:37:07 +0300

    ArrayListExercise

--- src/com/boraozgur/arrays/MobilePhone.java
+++ src/com/boraozgur/arrays/MobilePhone.java
@@ -27,6 +27,9 @@ public class MobilePhone {
 		if (foundposition < 0) {
 			System.out.println(oldContact.getName() + " was not found");
 			return false;
+		}else if(findContact(newContact.getName()) != -1 ) {
+			System.out.println("Contact with name "+ newContact.getName() +" already exists. Update was not successful");
+			return false;
 		}
 
 		this.myContacts.set(foundposition, newContact);
