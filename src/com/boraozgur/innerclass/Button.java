bcabe25add7bf3d6af413911a803f67af7bca465
testbora <boraozgurr@outlook.com>
2020-12-06 13:01:27 +0300

    InnerClass Example

---
+++ src/com/boraozgur/innerclass/Button.java
@@ -0,0 +1,28 @@
+package com.boraozgur.innerclass;
+
+public class Button {
+	private String title;
+	private OnClickListener onClickListener;
+
+	public Button(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setOnClickListener(OnClickListener onClickListener) {
+		this.onClickListener = onClickListener;
+	}
+
+	public void onClick() {
+		this.onClickListener.onClick(this.title);
+	}
+
+	// interface
+	public interface OnClickListener {
+		public void onClick(String title);
+	}
+
+}
